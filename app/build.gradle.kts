08b99ad285158809a5720d4209da99b1c0f4fc39
Alejandro <agomezp2003@gmail.com>
2026-01-03 12:21:51 -0500

    new version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "com.example.neosynth"
     minSdk = 28
     targetSdk = 35
-    versionCode = 1
-    versionName = "1.0"
+    versionCode = 2
+    versionName = "1.1.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
